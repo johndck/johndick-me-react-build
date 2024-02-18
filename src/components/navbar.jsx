06560ab943f5432db07c7f2af02b5ba90f93dbe0
John Turner Dick <JD@Johns-MacBook-Pro-2.local>
2024-02-18 15:33:47 +0000

    completed mobile nav menu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [isOpen, setOpen] = useState(false);
+
+  function openNav() {
+    setOpen(!isOpen);
+  }
+
+  function closeNav() {
+    setOpen(false);
+  }
+
   return (
     <div className="container navbar">
       {/*logo link to homepage */}
@@ -16,8 +27,41 @@ function Navbar() {
       {/*logo link to homepage */}
       <div>
         <div className="hamburger">
-          <button>&#9776;</button>
+          <button onClick={openNav}>&#9776;</button>
         </div>
+
+        {/* nav bar for smaller screens*/}
+
+        <div className={`nav-links ${isOpen ? "open" : ""}`}>
+          <button className="closebtn" onClick={closeNav}>
+            X{" "}
+          </button>
+
+          <ul>
+            <li>
+              <Link to="/" onClick={closeNav}>
+                Home
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/Evidence" onClick={closeNav}>
+                Past delivery
+              </Link>
+            </li>
+            <li>
+              <Link to="/Focus" onClick={closeNav}>
+                Current focus
+              </Link>
+            </li>
+            <li>
+              <Link to="/Shaped" onClick={closeNav}>
+                Influenced by
+              </Link>
+            </li>
+          </ul>
+        </div>
+
         <ul className="navbarElements">
           <li>
             <Link to="/Evidence">Past delivery</Link>
