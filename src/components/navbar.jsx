e9c8021ef7cbb99216e750155f785c1c1afbb762
John Turner Dick <JD@Johns-MacBook-Pro-2.local>
2024-02-19 20:47:49 +0000

    updated and improved the navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -14,67 +14,71 @@ function Navbar() {
   }
 
   return (
-    <div className="container navbar">
-      {/*logo link to homepage */}
-      <div>
-        <Link to="/">
-          <img
-            src="/assets/logo.svg"
-            alt="my site logo of 6 block colours"
-            className="logo"
-          />
-        </Link>
-      </div>
-      {/*logo link to homepage */}
-      <div>
-        <div className="hamburger">
-          <button onClick={openNav}>&#9776;</button>
+    <div className="navbar">
+      <div className="container navbarContent">
+        <div>
+          <Link to="/">
+            <img
+              src="/assets/logo.svg"
+              alt="my site logo of 6 block colours"
+              className="logo"
+            />
+          </Link>
         </div>
 
-        {/* nav bar for smaller screens*/}
+        {/*logo link to homepage */}
+        <div>
+          <div className="hamburger">
+            <button onClick={openNav}>&#9776;</button>
+          </div>
 
-        <div className={`nav-links ${isOpen ? "open" : ""}`}>
-          <button className="closebtn" onClick={closeNav}>
-            X{" "}
-          </button>
+          {/* nav bar for smaller screens*/}
 
-          <ul>
-            <li>
-              <Link to="/" onClick={closeNav}>
-                Home
-              </Link>
-            </li>
+          <div className={`nav-links ${isOpen ? "open" : ""}`}>
+            <button className="closebtn" onClick={closeNav}>
+              X{" "}
+            </button>
 
+            <ul>
+              <li>
+                <Link to="/" onClick={closeNav}>
+                  Home
+                </Link>
+              </li>
+
+              <li>
+                <Link to="/Evidence" onClick={closeNav}>
+                  Past delivery
+                </Link>
+              </li>
+              <li>
+                <Link to="/Focus" onClick={closeNav}>
+                  Current focus
+                </Link>
+              </li>
+              <li>
+                <Link to="/Shaped" onClick={closeNav}>
+                  Influenced by
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <ul className="navbarElements">
             <li>
-              <Link to="/Evidence" onClick={closeNav}>
-                Past delivery
-              </Link>
+              <NavLink to="/Evidence">Past delivery</NavLink>
             </li>
             <li>
-              <Link to="/Focus" onClick={closeNav}>
-                Current focus
-              </Link>
+              <NavLink to="/Focus">Current focus</NavLink>
             </li>
             <li>
-              <Link to="/Shaped" onClick={closeNav}>
-                Influenced by
-              </Link>
+              <NavLink to="/Shaped">Influenced by</NavLink>
             </li>
           </ul>
         </div>
-
-        <ul className="navbarElements">
-          <li>
-            <NavLink to="/Evidence">Past delivery</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Focus">Current focus</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Shaped">Influenced by</NavLink>
-          </li>
-        </ul>
       </div>
+
+      {/*logo link to homepage */}
     </div>
   );
 }
