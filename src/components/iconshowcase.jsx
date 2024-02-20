999e1846b8434f12bbfa792bb0215e51476ef40b
John Turner Dick <JD@Johns-MacBook-Pro-2.local>
2024-02-20 11:50:53 +0000

    added in past employers grid

--- src/components/iconshowcase.jsx
+++ src/components/iconshowcase.jsx
@@ -5,9 +5,84 @@
 function Showcase() {
   return (
     <div className="showcaseSection">
-      <div className="container showcase">
-        <div className="showcaseTitle">
-          <h2>As delivered in</h2>
+      <div className="container">
+        <div className="showcase showcaseTitle">
+          <h2>Past employers</h2>
+        </div>
+
+        <div className="showcaseLogocontainer">
+          <div className="showcaseLogoCard">
+            <div>
+              <img
+                src="/assets/RGA.png"
+                alt="Reinsurance Group of America"
+                className="showcaseIcon"
+              />
+            </div>
+          </div>
+          <div className="showcaseLogoCard">
+            <div>
+              <img
+                src="/assets/ESURE.png"
+                alt="Reinsurance Group of America"
+                className="showcaseIcon"
+              />
+            </div>
+          </div>
+          <div className="showcaseLogoCard">
+            <div>
+              <img
+                src="/assets/PrincesTrust.png"
+                alt="Reinsurance Group of America"
+                className="showcaseIcon"
+              />
+            </div>
+          </div>
+          <div className="showcaseLogoCard">
+            <div>
+              <img
+                src="/assets/FCA.png"
+                alt="Reinsurance Group of America"
+                className="showcaseIcon"
+              />
+            </div>
+          </div>
+          <div className="showcaseLogoCard">
+            <div>
+              <img
+                src="/assets/NetworkRail.png"
+                alt="Reinsurance Group of America"
+                className="showcaseIcon"
+              />
+            </div>
+          </div>
+          <div className="showcaseLogoCard">
+            <div>
+              <img
+                src="/assets/Homeserve.png"
+                alt="Reinsurance Group of America"
+                className="showcaseIcon"
+              />
+            </div>
+          </div>
+          <div className="showcaseLogoCard">
+            <div>
+              <img
+                src="/assets/MOD.png"
+                alt="Reinsurance Group of America"
+                className="showcaseIcon"
+              />
+            </div>
+          </div>
+          <div className="showcaseLogoCard">
+            <div>
+              <img
+                src="/assets/Capgemini.png"
+                alt="Reinsurance Group of America"
+                className="showcaseIcon"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
