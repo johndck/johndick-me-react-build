e0b741bc2430f427cfd5587de722366793a2f535
John Turner Dick <JD@Johns-MacBook-Pro-2.local>
2024-02-20 10:22:56 +0000

    made small improvement to hero section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -6,9 +6,11 @@ function Hero() {
   return (
     <div className="container hero">
       <h1>John Dick</h1>
+      <h4 className="problemText">Problem:</h4>
       <h2>
         Who am I<span className="redq">?</span>
       </h2>
+      <h4 className="solutionText">Solution:</h4>
       <p>
         {" "}
         <span className="orangehighlight">
