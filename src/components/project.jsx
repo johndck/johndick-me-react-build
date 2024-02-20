98321228d972c836ebd546d276df5d4d1c441337
John Turner Dick <JD@Johns-MacBook-Pro-2.local>
2024-02-20 17:29:42 +0000

    updated past evidence cards

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -2,18 +2,19 @@ function ProjectCard({ card }) {
   return (
     <div className="evidenceCard">
       <h2>{card.companyName}</h2>
+      <hr />
       <h3>
         <span className="evidenceChallenge">The challenge:</span>
       </h3>
       <p>{card.challenge}</p>
       <h3>
-        <span className="evidenceActions">My Actions</span>
+        <span className="evidenceActions">My Actions: </span>
       </h3>
       <p>{card.actions[0]}</p>
       <p>{card.actions[1]}</p>
       <p>{card.actions[2]}</p>
       <h3>
-        <span className="evidenceResults">Results</span>
+        <span className="evidenceResults">Results: </span>
       </h3>
       <p>{card.results[0]}</p>
       <p>{card.results[1]}</p>
