aa4d5b32537b9e01f238a83a900c401bb230b13e
John Turner Dick <JD@Johns-MacBook-Pro-2.local>
2024-02-22 21:15:34 +0000

    added accordian

--- src/components/accordianItem.jsx
+++ src/components/accordianItem.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+
 function AccordianItem({ item }) {
+  const [isOpen, setOpen] = useState(false);
+
+  function handleOpen() {
+    setOpen(!isOpen);
+  }
+
   return (
     <div className="accordianItem">
-      <div className="accordTitle">
-        <p>{item.id}</p>
+      <div
+        className="accordTitle"
+        onClick={() => {
+          handleOpen();
+        }}
+      >
+        <p className="accordianNumber">{item.id}</p>
         <p>{item.title}</p>
-        <p>-</p>
+        <p className="accordianController">{isOpen === true ? "-" : "+"}</p>
       </div>
-      <div>
+      <div
+        className="accordianAnswer"
+        style={{ display: isOpen ? "block" : "none" }}
+      >
         <p>{item.answer}</p>
       </div>
     </div>
