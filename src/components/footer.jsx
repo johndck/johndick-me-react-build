279996e9a313c34c0d924fa7a4940aaa985fac96
John Turner Dick <JD@Johns-MacBook-Pro-2.local>
2024-03-11 21:30:03 +0000

    updated styling issues

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -30,7 +30,7 @@ function Footer() {
             <a href="mailto:johnturnerdick@gmail.com">
               email: johnturnerdick@gmail.com
             </a>
-            <p>release version v1.02 | 10 Mar</p>
+            <p>release version v1.03 | 11 Mar</p>
             <p>Site: built by me</p>
             <p>Tech: React</p>
           </div>
