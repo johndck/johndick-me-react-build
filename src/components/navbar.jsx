afa7712919a090fb975fbf357c5e40e89812edcd
John Turner Dick <JD@Johns-MacBook-Pro-2.local>
2024-03-22 16:49:11 +0000

    new version of the site

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,84 +1,18 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { NavLink } from "react-router-dom";
+import TopNav from "./nav";
+import Navtitle from "./navtitle";
+import NavElements from "./navmenu";
 
 function Navbar() {
-  const [isOpen, setOpen] = useState(false);
-
-  function openNav() {
-    setOpen(!isOpen);
-  }
-
-  function closeNav() {
-    setOpen(false);
-  }
-
   return (
-    <header className="navbar">
-      <div className="container navbarContent">
-        <div>
-          <Link to="/">
-            <img
-              src="/assets/johndick.png"
-              alt="my site logo of 6 block colours"
-              className="logo"
-            />
-          </Link>
+    <header>
+      <nav>
+        <div className="relative container sm:mx-auto sm:max-w-4/5 md:mx-auto md:max-w-4/5 lg:mx-auto lg:max-w-4/5 xl:mx-auto xl:w-9/10 xl:max-w-1280">
+          <TopNav>
+            <Navtitle />
+            <NavElements />
+          </TopNav>
         </div>
-
-        {/*logo link to homepage */}
-        <div>
-          <div className="hamburger">
-            <button onClick={openNav}>&#9776;</button>
-          </div>
-
-          {/* nav bar for smaller screens*/}
-
-          <div className={`nav-links ${isOpen ? "open" : ""}`}>
-            <button className="closebtn" onClick={closeNav}>
-              X{" "}
-            </button>
-
-            <ul>
-              <li>
-                <Link to="/" onClick={closeNav}>
-                  Home
-                </Link>
-              </li>
-
-              <li>
-                <Link to="/Evidence" onClick={closeNav}>
-                  Past delivery
-                </Link>
-              </li>
-              <li>
-                <Link to="/Focus" onClick={closeNav}>
-                  Current focus
-                </Link>
-              </li>
-              <li>
-                <Link to="/Shaped" onClick={closeNav}>
-                  Influenced by
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <ul className="navbarElements">
-            <li>
-              <NavLink to="/Evidence">Past delivery</NavLink>
-            </li>
-            <li>
-              <NavLink to="/Focus">Current focus</NavLink>
-            </li>
-            <li>
-              <NavLink to="/Shaped">Influenced by</NavLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      {/*logo link to homepage */}
+      </nav>
     </header>
   );
 }
